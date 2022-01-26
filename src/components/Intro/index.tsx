1c6c923ccde766a7b49dc7cb4499b8747229d6ce
hashcott <duchanhstyle@gmail.com>
2022-01-26 12:46:14 +0700

    fix style Intro Web

--- src/components/Intro/index.tsx
+++ src/components/Intro/index.tsx
@@ -3,24 +3,24 @@ import React from 'react';
 import { IntroData } from './data';
 
 const FigureIntro: React.FC = ({ children }) => (
-  <figure className="grid grid-cols-2 gap-x-2 gap-y-4 md:grid-cols-4">
+  <figure className="grid grid-cols-2 gap-x-2 gap-y-4 lg:gap-x-6 lg:grid-cols-4">
     {children}
   </figure>
 );
 
 export const Intro = () => {
   return (
-    <section className="container bg-monochrome-white pt-14 md:pt-0">
+    <section className="container bg-monochrome-white mt-14 lg:pt-0">
       <FigureIntro>
         {IntroData &&
           IntroData.map(({ image, description }) => {
             return (
               <div
                 key={Math.random()}
-                className="flex rounded-lg bg-monochrome-light flex-col items-center text-center"
+                className="flex p-4 lg:px-2 lg:py-6 gap-y-2 rounded-lg bg-monochrome-light flex-col items-center"
               >
-                <img className="pt-4" src={useBaseUrl(image)} alt="" />
-                <p className="font-bold pb-4 text-dark-tiny leading-24 text-center px-4 mt-2 md:text-20 md:mt-4 md:leading-28">
+                <img src={useBaseUrl(image)} alt="" />
+                <p className="font-bold text-dark-tiny leading-24 text-center lg:text-20 lg:leading-28 lg:whitespace-nowrap">
                   {description}
                 </p>
               </div>
