1563ce8bf2434b0b2f6e4fd66b4703f26872516f
hashcott <duchanhstyle@gmail.com>
2022-01-26 12:46:48 +0700

    fix style FeatureList lg screen

--- src/components/FeatureList/index.tsx
+++ src/components/FeatureList/index.tsx
@@ -10,11 +10,11 @@ const FigureIntro: React.FC = ({ children }) => (
 
 export const FeatureList = () => {
   return (
-    <section className="container pt-14 lg:pt-44">
+    <section className="container mt-14 lg:mt-44">
       <h1 className="text-26 text-dark-tiny leading-32 font-medium text-center">
         Features our users love
       </h1>
-      <div className="grid mt-6 lg:mt-12 gap-y-8 grid-cols-1 lg:grid-cols-3">
+      <div className="grid mt-6 lg:mt-12 gap-y-8 lg:gap-x-10 lg:gap-y-12 grid-cols-1 lg:grid-cols-3">
         {FeatureData &&
           FeatureData.map(({ image, title, desc }) => {
             return (
@@ -23,7 +23,7 @@ export const FeatureList = () => {
                 className="flex flex-col items-center justify-center"
               >
                 <img src={useBaseUrl(image)} alt={title} />
-                <h3 className="text-16 font-bold lg:pt-6 leading-24 text-center mt-3 text-dark-tiny">
+                <h3 className="text-16 font-bold lg:mt-6 leading-24 text-center mt-3 text-dark-tiny">
                   {title}
                 </h3>
                 <p className="pt-1 px-4 lg:pt-2 text-16 leading-24 text-dark-brown text-center">
