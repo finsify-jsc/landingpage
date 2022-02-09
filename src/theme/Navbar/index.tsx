2b6a3681222917d1bcc1f011ac943c4ec99fd016
hashcott <duchanhstyle@gmail.com>
2022-02-09 12:07:19 +0700

    change lang

--- src/theme/Navbar/index.tsx
+++ src/theme/Navbar/index.tsx
@@ -34,20 +34,18 @@ const Navbar = () => {
   }
 
   const localeItems = locales.map((locale) => {
-    const to = `pathname://${alternatePageUtils.createUrl({
-      locale,
-      fullyQualified: false,
-    })}`;
+    const to =
+      locale === currentLocale ? `pathname:///` : `pathname://${locale}`;
     return {
+      locale,
       isNavLink: true,
       label: getLocaleLabel(locale),
       to,
       target: '_self',
       autoAddBaseUrl: false,
-      className: locale === currentLocale ? 'dropdown__link--active' : '',
+      className: locale === currentLocale ? 'dropdown__link--active' : 'hidden',
     };
   });
-
   console.log(localeItems);
 
   const { pathname } = useLocation();
@@ -60,7 +58,7 @@ const Navbar = () => {
   return (
     <header
       ref={navbarRef}
-      className={clsx('fixed top-0 inset-x-0 z-50 h-22', styles.header, {
+      className={clsx('fixed top-0 inset-x-0 z-50 h-20', styles.header, {
         [styles.headerHidden]: !isHeaderVisibleAfterScroll,
       })}
     >
@@ -190,17 +188,17 @@ const Navbar = () => {
         />
       </Head>
       <div className="absolute top-0 left-0 z-40 w-full bg-monochrome-white">
-        <nav className="flex items-center justify-between h-22 container">
+        <nav className="flex items-center justify-between h-20 container">
           <Link to={useBaseUrl('/')}>
-            <img src={useBaseUrl('/img/logo.svg')} alt="drill-logo" />
+            <img src={useBaseUrl('/img/logo.svg')} alt="Money Lover" />
           </Link>
           <div className="md:flex items-center">
-            <ul className="invisible md:visible fixed md:static flex flex-row gap-6 items-center">
+            <ul className="invisible md:visible fixed md:static flex flex-row gap-6 items-center mt-1">
               {links.map(({ to = '', label = '' }: any) => (
                 <li key={to}>
                   <Link
                     style={{ textDecoration: 'none' }}
-                    className={`flex items-center h-22
+                    className={`flex items-center
                       text-16 text-dark-brown
                       ${
                         pathname.includes(to.split('/')[0])
@@ -214,36 +212,65 @@ const Navbar = () => {
                   </Link>
                 </li>
               ))}
+              {localeItems &&
+                localeItems.map(({ to, label, locale }) => {
+                  console.log(useBaseUrl(to));
+
+                  return (
+                    <li>
+                      <Link
+                        style={{ textDecoration: 'none' }}
+                        target="_self"
+                        className={clsx(
+                          'text-16 font-medium text-dark-brown flex items-center gap-1',
+                          (pathname === '/' && locale === 'vi') ||
+                            (pathname === '/en' && locale === 'en')
+                            ? 'hidden'
+                            : '',
+                        )}
+                        to={useBaseUrl(to)}
+                      >
+                        <img
+                          className="w-6 inline-block"
+                          src={useBaseUrl(`/img/${locale}.svg`)}
+                          alt={label}
+                        />
+                        {label}
+                      </Link>
+                    </li>
+                  );
+                })}
+              {/* <li className="dropdown relative">
+                <button className="text-dark-brown py-2 px-6 rounded inline-flex items-center">
+                  <span className="mr-1">Ngôn ngữ</span>
+                  <svg
+                    className="fill-current h-4 w-4"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                  </svg>
+                </button>
+                <ul className="dropdown-menu w-full hidden absolute pt-1">
+                  {localeItems &&
+                    localeItems.map(({ to, label, target }) => {
+                      return (
+                        <li className="text-center">
+                          <Link
+                            style={{ textDecoration: 'none' }}
+                            target="_self"
+                            className="text-16 font-medium text-dark-brown"
+                            to={useBaseUrl(to)}
+                          >
+                            {label}
+                          </Link>
+                        </li>
+                      );
+                    })}
+                </ul>
+              </li> */}
             </ul>
-            <div className="dropdown inline-block relative">
-              <button className="text-dark-brown py-2 px-6 rounded inline-flex items-center">
-                <span className="mr-1">Ngôn ngữ</span>
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
-              </button>
-              <ul className="dropdown-menu w-full hidden absolute pt-1">
-                {localeItems &&
-                  localeItems.map(({ to, label, target }) => {
-                    return (
-                      <li className="text-center">
-                        <Link
-                          style={{ textDecoration: 'none' }}
-                          target="_self"
-                          className="text-16 font-medium text-dark-brown"
-                          to={useBaseUrl(to)}
-                        >
-                          {label}
-                        </Link>
-                      </li>
-                    );
-                  })}
-              </ul>
-            </div>
+
             <button
               aria-label="menu button"
               style={{
@@ -263,7 +290,7 @@ const Navbar = () => {
       {isNavbarVisible && (
         <nav className="visible md:invisible fixed w-full h-full left-0 top-14">
           <div className="bg-monochrome-white">
-            <div className="container pt-2 pb-6">
+            <div className="container mt-4 pt-2 pb-4">
               <ul>
                 {links.map(({ to = '', label = '' }: any) => (
                   <li className="border-t border-monochrome-medium-tint">
@@ -279,46 +306,37 @@ const Navbar = () => {
                     </Link>
                   </li>
                 ))}
+                <li className="dropdown border-t border-monochrome-medium-tint relative w-full">
+                  <button className="flex items-center text-16 font-medium text-dark-brown leading-24 py-4 w-full">
+                    <span className="mr-1">Ngôn ngữ</span>
+                    <svg
+                      className="fill-current h-4 w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                    </svg>
+                  </button>
+                  <ul className="dropdown-menu w-full hidden absolute pt-1">
+                    {localeItems &&
+                      localeItems.map(({ to, label, target }) => {
+                        return (
+                          <li className="text-center">
+                            <Link
+                              style={{ textDecoration: 'none' }}
+                              target="_self"
+                              className="text-16 font-medium text-dark-brown"
+                              to={useBaseUrl(to)}
+                            >
+                              {label}
+                            </Link>
+                          </li>
+                        );
+                      })}
+                  </ul>
+                </li>
               </ul>
             </div>
-            {/* <div className="dropdown inline-block relative">
-              <button className="bg-gray-300 text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
-                <span className="mr-1">Dropdown</span>
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />{' '}
-                </svg>
-              </button>
-              <ul className="dropdown-menu absolute hidden text-gray-700 pt-1">
-                <li className="">
-                  <a
-                    className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                    href="#"
-                  >
-                    One
-                  </a>
-                </li>
-                <li className="">
-                  <a
-                    className="bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                    href="#"
-                  >
-                    Two
-                  </a>
-                </li>
-                <li className="">
-                  <a
-                    className="rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap"
-                    href="#"
-                  >
-                    Three is the magic number
-                  </a>
-                </li>
-              </ul>
-            </div> */}
           </div>
           <div
             className="w-full h-full bg-monochrome-black bg-opacity-50"
