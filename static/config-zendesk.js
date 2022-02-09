e4f739912ee7a3b3e338a409183382d6691d28ae
hashcott <duchanhstyle@gmail.com>
2022-02-09 15:18:21 +0700

    setting config zendesk

--- static/config-zendesk.js
+++ static/config-zendesk.js
@@ -2,9 +2,7 @@ window.zESettings = {
   webWidget: {
     color: { theme: '#2DB84C', launcherText: '#ffffff' },
     offset: {
-      horizontal: '50px',
-      vertical: '100px',
-      mobile: { horizontal: '10px', vertical: '100px' },
+      mobile: { horizontal: '0px', vertical: '88px' },
     },
   },
 };
